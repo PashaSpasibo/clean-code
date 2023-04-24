4c691b189efe5400a8db9756821a2f2c5689aa40
rb.nmd <pav.tro98@icloud.com>
2023-04-25 01:02:07 +0400

    fix(extended 1.2): add "alt" for media

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="task-form__delete-button button";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.alt="delete-icon"
   deleteButtonImg.className="task-form__delete-img"
   deleteButton.appendChild(deleteButtonImg);
 
